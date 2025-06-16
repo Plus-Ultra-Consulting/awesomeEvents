2a6d039b56d500b9b10a7ce7591f971dc8bb371f
COTE-LAPYX <andreyhilj@gmail.com>
2025-06-16 11:56:01 +0300

    Initial commit. Created project (project structure) with Spring Initializer. Added Dependencies that correspond to our tech stack.

---
+++ src/main/java/fr/leplusultra/awesomeevents/artawesomeevents/AwesomeEventsApplication.java
@@ -0,0 +1,13 @@
+package fr.leplusultra.awesomeevents.artawesomeevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeEventsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/leplusultra/awesomeevents/artawesomeevents/AwesomeEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.leplusultra.awesomeevents.artawesomeevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
